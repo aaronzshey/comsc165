64b0fd2d0b5acc56539bb39ac7e768c3cfeed9f6
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-01-30 20:55:08 -0800

    updating test file

--- test.cpp
+++ test.cpp
@@ -3,16 +3,4 @@ using namespace std;
 int main() {
   cout << "Hello World"
        << "\n";
-  cout << "Type something!"
-       << "\n";
-
-  string filename;
-  getline(cin, filename);
-
-  cout << "You type" << filename << "\n";
-
-  cout << "cogisgood";
-  if (1 == 1) {
-    cout << "COGG";
-  };
 }
