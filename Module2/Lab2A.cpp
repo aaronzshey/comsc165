e16ffa0e8f161c2da8079cdb6f1a7379eb7f160f
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-02-07 19:22:08 -0800

    Perfecting Lab2A

--- Module2/Lab2A.cpp
+++ Module2/Lab2A.cpp
@@ -1,26 +1,24 @@
 // Example program
 #include <iostream>
-#include <string>
 using namespace std;
-void getRadius(double&);
+void getRadius(double &);
 void showArea(double);
 
 int main() {
 
-double radius;
-getRadius(radius);
-showArea(radius);
+  double radius;
+  getRadius(radius);
+  showArea(radius);
 }
 
-void getRadius(double& r) {
+void getRadius(double &r) {
 
-cout << "Enter the radius of your circle: ";
-cin >> r;
-
+  cout << "Enter the radius of your circle: ";
+  cin >> r;
 }
 
-void showArea(double r) {    
-double area;
-area = M_PI * pow(r,2);
-cout << "The area of the circle is " << area;
+void showArea(double r) {
+  double area;
+  area = 3.14159 * r * r;
+  cout << "The area of the circle is " << area;
 }
