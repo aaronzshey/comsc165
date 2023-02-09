d49b379cf599b27b9e0f0903ddaba5b5c8440a50
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-02-08 19:17:59 -0800

    Finished module 2

--- Module2/circles.cpp
+++ Module2/circles.cpp
@@ -1,46 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void getRadius(double&);
+void getRadius(double &);
 void findArea(double);
 void findDiameter(double);
 void findCircumference(double);
 void print(double);
 
-
 int main() {
-    double radius;
-    print(radius);
+  double radius;
+	getRadius(radius);
+  print(radius);
 }
 
-void getRadius(double& radius) {
-    cout << "Enter the radius of a circle: ";
-while (true) {
+void getRadius(double &radius) {
+  do {
+    cout << "Enter the radius for a circle: ";
     cin >> radius;
-    if (radius < 0) {
-        cout << "Error: only positive values allowed. Try again.\n";
-        cout << "Enter the radius of a circle: ";
-        cin >> radius;
-    } else break;
-    
-} 
-
+    if (radius <= 0) {
+      cout << "Error - only positive values allowed.  Try again.\n";
+      continue;
+    } else
+      cout << "Circle radius: " << radius << "\n";
+    break;
+  } while (true);
 }
 void findArea(double radius) {
-    double area = 3.14159 * radius * radius;
-    cout << "Circle area: " << area << "\n";
+  double area = 3.14159 * radius * radius;
+  cout << "Circle area: " << area << "\n";
 }
 void findDiameter(double radius) {
-    double diameter =  2 * radius;
-    cout << "Circle diameter: " << diameter << "\n";
+  double diameter = 2 * radius;
+  cout << "Circle diameter: " << diameter << "\n";
 }
 void findCircumference(double radius) {
-    double circumference = 2 * 3.14159 * radius;
-    cout << "The circumference is: " << circumference << "\n";
+  double circumference = 2 * 3.14159 * radius;
+  cout << "Circle circumference: " << circumference << "\n";
 }
 void print(double input) {
-    double input;
-    getRadius(input);
-    findArea(input);
-    findCircumference(input);
+  findArea(input);
+  findDiameter(input);
+  findCircumference(input);
 }
