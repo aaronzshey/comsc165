573fcd190f1763fcd70bc1b6274cbe6945166985
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-02-14 21:22:55 -0800

    started on assignment 3

---
+++ Module3/vectors.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+
+void load(vector<double> &, string = "numbers.txt");
+double findHighest(const vector<double> &);
+double findAverage(const vector<double> &);
+void sort(vector<double> &);
+void print(const vector<double> &);
+
+int main() {
+string filename;
+cout << "Enter a file name: ";
+getline(cin, filename);
+
+vector<double> vec;
+load(vec);
+
+
+}
+
+void load(vector<double> &input, string = "numbers.txt") {
+				
+}
+double findHighest(const vector<double> &);
+double findAverage(const vector<double> &);
+void sort(vector<double> &);
+void print(const vector<double> &);
+
