7b7f7663db3a192ed4201ae684a00c74e37cdd5f
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-02-19 17:11:31 -0800

    testing a little vector printing code

--- test.cpp
+++ test.cpp
@@ -1,6 +1,20 @@
+#include "Module3/Module3Functions.h"
+#include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
+
 int main() {
   cout << "Hello World"
        << "\n";
+  vector<int> v;
+  ifstream i;
+  i.open("a");
+  int o;
+  while (i >> o)
+   	v.push_back(o); 
+	
+	i.close();
+	cout << v[0] << "asdf";
+  printVec(v);
 }
