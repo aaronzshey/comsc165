b8ce19e14542f89a4088cf755ae6a254074cce6b
CarlyRaeJepsenStan <44917034+CarlyRaeJepsenStan@users.noreply.github.com>
2023-02-23 10:39:11 -0800

    Update pointers.cpp

--- Module4/pointers.cpp
+++ Module4/pointers.cpp
@@ -2,19 +2,32 @@
 using namespace std;
 
 int *getSize();
-int *makeArray();
-
+int *makeArray(int*);
+int *fillArray();
+int *findAverage();
+void print();
 
 int main() {
-	
+
 }
 
 int *getSize() {
+int *radius;
+radius = new int;
   cout << "Enter the radius for a circle: ";
-  cin >> radius;
-  while (radius <= 0) {
+  cin >> *radius;
+  while (*radius <= 0) {
     cout << "Error - only positive values allowed.  Try again.\n";
     cout << "Enter the radius for a circle: ";
-    cin >> radius;
+    cin >> *radius;
   }
+	return radius;
 }
+
+int *makeArray(int *size) {
+return new int[*size];
+	
+	
+}
+
+int *fillArray()
