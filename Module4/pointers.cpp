052248c5ef27000162cbeae9e270cf2fc523631e
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-02-23 19:31:12 -0800

    Finished assignment 4 for now, will check later.

--- Module4/pointers.cpp
+++ Module4/pointers.cpp
@@ -2,32 +2,69 @@
 using namespace std;
 
 int *getSize();
-int *makeArray(int*);
-int *fillArray();
-int *findAverage();
-void print();
+int *makeArray(int *);
+int *fillArray(int *, int *);
+float *findAverage(int *, int *);
+void print(int *, int *, float *);
 
 int main() {
-
+  int *size = getSize(); //* tells cpp to find the value located at the memory
+                         // address returned by getSize;
+  int *arr = makeArray(size);
+  //* not needed on rvalue because lvalue is also a pointer
+  *arr = *fillArray(arr, size);
+  print(arr, size, findAverage(arr, size));
 }
 
 int *getSize() {
-int *radius;
-radius = new int;
-  cout << "Enter the radius for a circle: ";
+  int *radius;
+  radius = new int;
+  cout << "Enter the number of integers you want to average: ";
   cin >> *radius;
   while (*radius <= 0) {
     cout << "Error - only positive values allowed.  Try again.\n";
-    cout << "Enter the radius for a circle: ";
+    cout << "Enter the number of integers you want to average:";
     cin >> *radius;
   }
-	return radius;
+  return radius;
 }
 
 int *makeArray(int *size) {
-return new int[*size];
-	
-	
+  int *a;
+  a = new int[*size];
+  return a;
 }
 
-int *fillArray()
+int *fillArray(int *arr, int *size) {
+
+  for (int i = 0; i < *size; i++) {
+
+    int in;
+    cout << "Enter #" << i + 1 << ": ";
+    cin >> in;
+    *(arr + i) = in;
+  }
+  return arr;
+};
+
+float *findAverage(int *arr, int *size) {
+  float *a;
+  a = new float;
+
+  for (int i = 0; i < *size; i++) {
+
+    *a += *(arr + i);
+  }
+  *a /= *size;
+  return a;
+}
+
+void print(int *arr, int *size, float *average) {
+  cout << "Array contents: ";
+
+  for (int i = 0; i < *size; i++) {
+    cout << *(arr + i) << " ";
+  }
+
+  cout << "\nAverage: " << *average;
+}
