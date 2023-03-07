9f10e8ed052e8093b3d8bb0de3863a0cf6a7b1b1
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-03-06 20:54:27 -0800

    Finished module 5

--- Module5/countThemUp.cpp
+++ Module5/countThemUp.cpp
@@ -1,9 +1,36 @@
-#include <iostream>
-#include <fstream>
 #include <cctype>
+#include <fstream>
+#include <iostream>
 using namespace std;
-
-
 int main() {
-	
+  cout << "Enter the name of a file: ";
+  ifstream ii;
+  string filename, fileCont;
+  getline(cin, filename);
+  ii.open(filename);
+  if (ii.is_open()) {
+    int numUpper = 0,
+    numLower = 0,
+		numDigit = 0,
+    numPunct = 0;
+    while (getline(ii, fileCont)) {
+      const char *c = fileCont.c_str();
+      for (int i = 0; c[i] != '\0'; i++) {
+        if (isupper(c[i])) {
+          numUpper++;
+        } else if (islower(c[i])) {
+          numLower++;
+        } else if (isdigit(c[i])) {
+          numDigit++;
+        } else if (ispunct(c[i])) {
+          numPunct++;
+        } else {
+        }
+      }
+    }
+    cout << "Upper: " << numUpper << "\nLower: " << numLower
+         << "\nDigits: " << numDigit << "\nPunctuation: " << numPunct << "\n";
+  } else {
+    cout << "Error. File not found.";
+  }
 }
