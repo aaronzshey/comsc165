7b4712e63306843bfb0f10f762931576bd408cc6
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-03-06 16:37:58 -0800

    Finished the first lab

--- Module5/Lab5A.cpp
+++ Module5/Lab5A.cpp
@@ -1,12 +1,11 @@
-#include <cctype>
 #include <iostream>
 using namespace std;
 int mystrlen(const char *);
 int main() {
-  const int SIZE = 100;
+  const int SIZE = 14;
   char str[SIZE];
   cout << "Enter a string: ";
-  cin.getline(str, SIZE);
+  cin.getline(str, SIZE - 1);
   int length = mystrlen(str);
   cout << "The string you entered contains " << length << " character(s)."
        << endl;
