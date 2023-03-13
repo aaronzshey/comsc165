88850abd99b62e60361f89ee476ddd6f8d4d31dd
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-03-13 15:41:52 -0700

    working on assignment 6

--- Module6/salsas.cpp
+++ Module6/salsas.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+struct Salsa {
+string name;
+int sold;
+};
 
 int main() {
-	
+vector<string> names={"mild", "medium", "sweet", "hot", "zesty"};
+Salsa salsaList[names.size()];
+
+for (int i = 0; i < names.size(); i++) {
+int sales = 0;
+cout << "Enter jars of " << names[i] << " sold: ";
+cin >> sales;
+salsaList[i]={names[i], sales};
+}
+
+//sort the list?!
+
+for (int i = 0; i < names.size(); 
+cout << salsaList[1].name;
+
 }
