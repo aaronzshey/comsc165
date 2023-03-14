7170507f1f4e89573e3d202899bd4d75cb9389fe
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-03-13 17:08:23 -0700

    still working on sort code

--- Module6/salsas.cpp
+++ Module6/salsas.cpp
@@ -3,24 +3,36 @@
 #include <vector>
 using namespace std;
 struct Salsa {
-string name;
-int sold;
+  string name;
+  int sold;
 };
 
 int main() {
-vector<string> names={"mild", "medium", "sweet", "hot", "zesty"};
-Salsa salsaList[names.size()];
+  vector<string> names = {"mild", "medium", "sweet", "hot", "zesty"};
+  int sz = names.size();
+  Salsa salsaList[sz];
 
-for (int i = 0; i < names.size(); i++) {
-int sales = 0;
-cout << "Enter jars of " << names[i] << " sold: ";
-cin >> sales;
-salsaList[i]={names[i], sales};
-}
+  for (int i = 0; i < sz; i++) {
+    int sales = 0;
+    cout << "Enter jars of " << names[i] << " sold: ";
+    cin >> sales;
+    salsaList[i] = {names[i], sales};
+  }
 
-//sort the list?!
+  // sort the list?!
+  for (int i = 0; i < sz; i++) {
+    int second = salsaList[i + 1].sold;
+    int first = salsaList[i].sold;
+    if (first > second) {
+      int temp = first;
+      first = second;
+      second = temp;
+    } else {
+      continue;
+    }
+  }
 
-for (int i = 0; i < names.size(); 
-cout << salsaList[1].name;
-
+  for (int i = 0; i < sz; i++) {
+    cout << salsaList[i].sold;
+  }
 }
