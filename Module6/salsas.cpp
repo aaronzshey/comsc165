fba9ef6043862bf7f098c0df32f0dcbb0efc57e0
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-03-13 21:39:27 -0700

    FINISHED THIS DAMN ASSIGNMENT

--- Module6/salsas.cpp
+++ Module6/salsas.cpp
@@ -11,21 +11,31 @@ int main() {
   vector<string> names = {"mild", "medium", "sweet", "hot", "zesty"};
   int sz = names.size();
   Salsa salsaList[sz];
-	int sum=0;
+  int sum = 0;
   for (int i = 0; i < sz; i++) {
     int sales = 0;
     cout << "Enter jars of " << names[i] << " sold: ";
     cin >> sales;
-		sum += sales;
+    sum += sales;
     salsaList[i] = {names[i], sales};
   }
-cout << "Salsa Name: Amount Sold";
-
-string max;
+  cout << "Salsa Name: Amount Sold"
+       << "\n\n";
 
   for (int i = 0; i < sz; i++) {
-cout << salsaList[i].name << " " << salsaList[i].sold << "\n"; 
+    cout << salsaList[i].name << " " << salsaList[i].sold << "\n";
+  }
 
+  cout << "\n"
+       << "Total sales: " << sum << "\n";
+
+  Salsa max = salsaList[0];
+
+  for (int i = 0; i < sz - 1; i++) {
+
+    if (salsaList[i].sold > max.sold) {
+      max = salsaList[i];
+    }
+  }
+  cout << "Highest selling salsa: " << max.name;
 }
-cout << "\n" << "Total sales: " << sum;
-	}
