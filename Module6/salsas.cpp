ab346360242566d6f91a87b652f58a1ebc912e80
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-03-13 21:11:22 -0700

    Still working on the assignment

--- Module6/salsas.cpp
+++ Module6/salsas.cpp
@@ -11,28 +11,21 @@ int main() {
   vector<string> names = {"mild", "medium", "sweet", "hot", "zesty"};
   int sz = names.size();
   Salsa salsaList[sz];
-
+	int sum=0;
   for (int i = 0; i < sz; i++) {
     int sales = 0;
     cout << "Enter jars of " << names[i] << " sold: ";
     cin >> sales;
+		sum += sales;
     salsaList[i] = {names[i], sales};
   }
+cout << "Salsa Name: Amount Sold";
 
-  // sort the list?!
-  for (int i = 0; i < sz; i++) {
-    int second = salsaList[i + 1].sold;
-    int first = salsaList[i].sold;
-    if (first > second) {
-      int temp = first;
-      first = second;
-      second = temp;
-    } else {
-      continue;
-    }
-  }
+string max;
 
   for (int i = 0; i < sz; i++) {
-    cout << salsaList[i].sold;
-  }
+cout << salsaList[i].name << " " << salsaList[i].sold << "\n"; 
+
 }
+cout << "\n" << "Total sales: " << sum;
+	}
