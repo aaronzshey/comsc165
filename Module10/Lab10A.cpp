4d23bd6401dd838c46d5e580cc52416d05dbadb7
CarlyRaeJepsenStan <711flametrooper@gmail.com>
2023-04-24 20:37:57 -0700

    working on week 10

---
+++ Module10/Lab10A.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Foo {
+protected:
+  int x, y;
+
+public:
+  Foo() {
+    x = 0;
+    y = 0;
+  }
+  Foo(int a, int b) {
+    x = a;
+    y = b;
+  }
+  void setX(int a) { x = a; }
+  void setY(int b) { y = b; }
+  int getX() const { return x; }
+  int getY() const { return y; }
+};
+class Bar : public Foo {
+protected:
+  int z;
+
+public:
+  Bar(int a, int b, int c) : Foo(a, b) { z = c; }
+  void setZ(int c) { z = c; }
+  int getZ() const { return z; }
+  int getAverage() const {
+    int sum, average;
+    sum = x + y + z;
+    average = sum / 3;
+    return average;
+  }
+  int getSum() const {
+    int sum;
+    sum = x + y + z;
+    return sum;
+  }
+};
+
+int main() {
+  Bar b(8, 6, 5);
+  cout << "Average: " << b.getAverage() << "\n"
+       << "Sum: " << b.getSum();
+}
